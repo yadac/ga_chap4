38d68fbfd58a44c284657bd7365c6f78ac50b4f4
Yosuke Adachi <yosuke.adachi@avanade.com>
2025-07-03 22:35:15 +0900

    fix typo

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 // EveOrOdd checks if a number is even or odd
 func TestEvenOrOdd(t *testing.T){
-	result := EveOrOdd(10)
+	result := EvenOrOdd(10)
 	if result != "even" {
 		t.Error("expected: even, actual: %s", result)
 	}
